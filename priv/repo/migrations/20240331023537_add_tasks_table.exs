a46eb819689fdb18a3c7a4e0a583d5a16efb2b23
Guilherme Azevedo <guiazevedo.deb@gmail.com>
2024-04-02 19:36:48 -0300

    refactor: update in table relation.

--- priv/repo/migrations/20240331023537_add_tasks_table.exs
+++ priv/repo/migrations/20240331023537_add_tasks_table.exs
@@ -7,7 +7,7 @@ defmodule GeckoApi.Repo.Migrations.AddTasksTable do
       add :title, :string
       add :description, :string
       add :completed, :boolean, default: false
-      add :user_id, references(:users, type: :uuid)
+      add :user_id, references(:users, type: :uuid, on_delete: :delete_all), null: false
 
       timestamps()
     end
