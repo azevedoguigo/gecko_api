765daf2b523980e8e0fea9e7594c40ca4c3b4a1a
Guilherme Azevedo <guilhermeazevedo.deb@gmail.com>
2024-07-08 01:06:26 -0300

    chore: back to phoenix 1.7.12 version.

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule GeckoApi.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.7.14"},
+      {:phoenix, "~> 1.7.12"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.10"},
       {:postgrex, ">= 0.0.0"},
